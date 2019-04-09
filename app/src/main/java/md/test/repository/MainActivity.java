9032d07274335daea731fdb33f1b5b52017a4dea
Nicolae Cozma <koleacozma@gmail.com>
2019-04-09 16:19:28 +0300

    0.0.4

--- app/src/main/java/md/test/repository/MainActivity.java
+++ app/src/main/java/md/test/repository/MainActivity.java
@@ -1,7 +1,11 @@
 package md.test.repository;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import md.test.toasterlibrary.ToasterMessage;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToasterMessage a = new ToasterMessage();
+        a.sEncript(this);
     }
 }
