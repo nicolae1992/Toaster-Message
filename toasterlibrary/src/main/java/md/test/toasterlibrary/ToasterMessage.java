b941e1d2fbac292ad6824e59c27e86d51d54c86d
Nicolae Cozma <koleacozma@gmail.com>
2019-04-09 16:06:01 +0300

    setProguard

--- toasterlibrary/src/main/java/md/test/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/md/test/toasterlibrary/ToasterMessage.java
@@ -5,8 +5,11 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message) {
+    public static void s1(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void sEncript(Context c ){
 
     }
 }
